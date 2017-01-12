742628d007bb613659152cb86be7a11e9bd73450
xiedongzhou <xiedongzhou1988@163.com>
2017-01-12 15:35:31 +0800

    filter out reply message; add message floor info

--- getArticle.py
+++ getArticle.py
@@ -13,34 +13,43 @@ def openSoup(url,code):
     soup = BeautifulSoup(content,'lxml',from_encoding=code)
     return soup
 
-def getContentFromDiv(contents):
-    s = ""
-    for content in contents:
-        try:
-            s += content
-        except:
-            pass
-    
-    s = s.lstrip().rstrip()
-    if len(s) < 50: # filter out short ones
-        return ""
-    else:
-        return "    "+s+"\r\n"+"\r\n"
+def IsValidContent(content): # filter out invalid ones
+    if len(content) < 100:  # filter out  too short ones
+        return False
+    fstSeg = content[:40]
+    if u'作者：' in fstSeg and u'回复日期：' in fstSeg:
+        return False
+    return True
 
+def getContentFromDiv(contents):
+    s = "".join(contents).strip()
+    return s + (os.linesep + os.linesep) if IsValidContent(s) else ""
+    
+def getFloorFromDiv(contents):
+    s = ''.join(contents).strip()
+    parts = s.split('|')
+    floor = '-'.join([item.strip() for item in parts if u'楼' in item])
+    decostr = '_-_' * 12
+    return '%s %s %s%s' % (decostr, floor, decostr, os.linesep)
+    
 def readHtml(soup,fp,authname):
     pageContent = ""
     item = soup.find(name='div', attrs={'class':'bbs-content clearfix'})
     if item != None:
-        pageContent += getContentFromDiv(item.contents)
+        pageContent += getContentFromDiv(item.strings)
 
     items = soup.findAll(name='div', attrs={'class':'atl-item'})
     for item in items:
         userItem = item.find(name='a', attrs={'class':'js-vip-check'})
         if userItem == None or userItem.contents[0] != authname:
             continue
-
+        
         contentItem = item.find(name='div', attrs={'class':'bbs-content'})
-        pageContent += getContentFromDiv(contentItem.contents)
+        contentPart  = getContentFromDiv(contentItem.strings)
+        if contentPart:
+            floorItem = item.find(name='div', attrs={'class':'atl-reply'})
+            floorPart = getFloorFromDiv(floorItem.strings)
+            pageContent += floorPart + contentPart
     
     fp.write(pageContent)
    
@@ -70,6 +79,7 @@ def getHtml(url):
         soup = openSoup(url,'utf-8')
         authname, title = getAuthor(soup)
         readHtml(soup,fp,authname)
+        fp.flush()
         print 'PAGE#%d OK' % p
         url = getNextPage(soup)
         if url == 'OVER' :
@@ -78,7 +88,13 @@ def getHtml(url):
        
     print '*** Article completely fetched. ***'
     fp.close()
-    if title: os.rename(filename, title+".txt")
+    if title:
+        newname = title+".txt"
+        if os.path.isfile(newname):
+            os.remove(newname)
+        os.rename(filename, newname)
+        filename = newname
+    print os.linesep, 'File name is: [%s]' % filename
 
 if __name__ == '__main__':
     getHtml('http://bbs.tianya.cn/post-free-2071655-1.shtml')
